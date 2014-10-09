773d78a11f1bf27898ac948ddee94b3b2282b2fb
maddymcadams <madeleinemcadams@gmail.com>
2014-10-08 20:59:13 -0400

    ToggleButton

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
   console.log("OMG JAVASCRIPT");
-  $("#celebs tbody tr:even").addClass("zebra");
+  $('#celebs tbody tr:even').addClass('zebra');
+
+
+	$('#toggleButton').click(function(){
+		$('#disclaimer').toggle();
+		if($('#disclaimer').is(':visible')) {
+			//if true
+			$(this).val('Show less!')
+			//this (=#toggleButon) value will be changed
+			//to hide
+		}
+		else {
+			$(this).val('Show more!');
+			//if false change value to show
+		}
+	})
+
+	$("input type='button' id='toggleButton' value='Click to hide or show disclaimer!'/>").insertAfter('#disclaimer');
+
+	$('#toggleButton').click(function(){
+		$('disclaimer').toggle();
+	})
+
 });
